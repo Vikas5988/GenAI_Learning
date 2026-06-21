54dd67ae8f05523907e48f7b394e4cf85b40a260
Vikas <Vikas.singh@learning.com>
2026-06-21 12:07:51 +0100

    06_chatbot with system and AI Message

--- GenAI_Lab1/chat_models/06_chatbot_langchain.py
+++ GenAI_Lab1/chat_models/06_chatbot_langchain.py
@@ -1,6 +1,7 @@
 #import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import AIMessage, SystemMessage, HumanMessage
 
 load_dotenv()  # ← automatically finds .env in current or parent folder
 #GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY") /Not Required
@@ -8,16 +9,16 @@ load_dotenv()  # ← automatically finds .env in current or parent folder
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 #model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite",temperature=.9,max_tokens=200) #WITH TOKEN LIMIT
 
-messages=[]
+messages=[
+    SystemMessage(content = "You are an expert in Devops and AI ")
+]
 
 print("---Welcome to the Chatbot---- Enter 0 to exit")
 while True:
     prompt=input("You: ")
     if prompt == "0":
         break
-    messages.append(prompt)
+    messages.append(HumanMessage(content=prompt))
     response = model.invoke(messages)
-    messages.append(response.content)
+    messages.append(AIMessage(content=response.content))
     print("Bot: ",response.content)
-    
-print(messages)
