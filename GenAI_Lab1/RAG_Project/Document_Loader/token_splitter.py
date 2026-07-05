1538179695071f70d21d03fa54578b43e5de58a4
Vikas <Vikas.singh@learning.com>
2026-07-05 18:09:10 +0100

    token and recursive text splitter

---
+++ GenAI_Lab1/RAG_Project/Document_Loader/token_splitter.py
@@ -0,0 +1,21 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import TokenTextSplitter
+
+Splitter = TokenTextSplitter(
+    chunk_size = 200,
+    chunk_overlap = 5
+)
+
+data=PyPDFLoader("Document_Loader/aws_lambda_guide.pdf")
+
+docs=data.load()
+
+chunks = Splitter.split_documents(docs)
+
+print(len(chunks))
+print(docs[6].page_content)
+
+
+
+
+
