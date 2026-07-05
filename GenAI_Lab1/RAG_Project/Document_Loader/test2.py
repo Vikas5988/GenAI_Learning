6f7fd4cf1b1ca95c647339d8b7cda5f3aa73636f
Vikas <Vikas.singh@learning.com>
2026-07-05 16:30:12 +0100

    pdf loader in RAG

--- GenAI_Lab1/RAG_Project/Document_Loader/test2.py
+++ GenAI_Lab1/RAG_Project/Document_Loader/test2.py
@@ -12,5 +12,7 @@ print(len(docs))
 
 print(docs[0].page_content)
 
-print(docs[4].page_content)
+print(docs[0])
+
+#print(docs[4].page_content)
 
--- GenAI_Lab1/RAG_Project/main_text.py
+++ GenAI_Lab1/RAG_Project/main_text.py
@@ -1,14 +1,13 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import TextLoader
 
 from dotenv import load_dotenv
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
 
-data=PyPDFLoader("Document_Loader/aws_lambda_guide.pdf")
-
+data=TextLoader("RAG_Project/Document_Loader/RHEL_Concept.txt",encoding="utf-8")
 docs=data.load()
 
 template = ChatPromptTemplate.from_messages([
@@ -16,8 +15,7 @@ template = ChatPromptTemplate.from_messages([
     "Human","{data}"
 ])
 
-#prompt=template.format(data=docs[1].page_content)
-prompt=template.format(data=docs)
+prompt=template.format(data=docs[0].page_content)
 
 result=model.invoke(prompt)
 print(result.content)
